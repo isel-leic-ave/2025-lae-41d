713e55ebf4b3448b314a87c87ebdfcf63adff23a
fgopassos <fernanda.passos@isel.pt>
2025-02-20 13:51:36 +0000

    Exemplos de dependências e interoperabilidade entre Java e Kotlin.

---
+++ week01/sample01-dependencies/Foo.java
@@ -0,0 +1,3 @@
+class X { public void print() { System.out.println("I am X, v3"); }}
+class Y { public void print() { System.out.println("I am Y"); }}
+class Z { public void print() { System.out.println("I am Z"); }}
