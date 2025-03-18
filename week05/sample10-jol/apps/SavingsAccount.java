29c0210f8c3e19df68f9b935fe3675676d962bc6
fgopassos <fernanda.passos@isel.pt>
2025-03-18 10:23:47 +0000

    Exemplos de classes para teste com JOL.

---
+++ week05/sample10-jol/apps/SavingsAccount.java
@@ -0,0 +1,10 @@
+package apps;
+
+public class SavingsAccount {
+    private static int accountsCount;
+    private short accountCode;
+    private String holderName;
+    private long balance;
+    private boolean isActive;
+    private final double interestRate = 0.02;
+}
