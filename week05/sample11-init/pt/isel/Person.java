97408ea27d58169084d9c4334ce9bd8db13d9f3e
fgopassos <fernanda.passos@isel.pt>
2025-03-18 10:24:58 +0000

    Exemplos para analisar o método construtor (init).

---
+++ week05/sample11-init/pt/isel/Person.java
@@ -0,0 +1,5 @@
+package pt.isel;
+
+// In command line, compile with: javac Person.java
+// to generate the Person.class file with bytecode
+class Person {}
---
+++ week05/sample11-init/pt/isel/Test.java
@@ -0,0 +1,5 @@
+package pt.isel;
+
+public class Test {
+    Person p = new Person();
+}
